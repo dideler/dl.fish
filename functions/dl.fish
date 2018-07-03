2067e9ae53778bcab76ff6166794cceb1a8632db
Dennis Ideler <ideler.dennis@gmail.com>
2018-07-03 23:48:24 +0100

    Add basic functionality
    - Download media from several URLs
    - Override existing configs for youtube-dl (don't want interference)
    - Save file with time of download, not time of upload
    - Save media as MP3
    - Save files to $HOME/Music (for now) and named after upload title
    - Do not show the usual verbose logging

--- functions/dl.fish
+++ functions/dl.fish
@@ -1,2 +1,15 @@
-function fish-dl -d "Wrapper around youtube-dl to easily download media"
+function dl --description "Opinionated alias for youtube-dl"
+    for path in $argv
+        __y_download $path
+    end
+end
+
+function __dl_download --argument path
+    youtube-dl  --ignore-config \
+                --quiet \
+                --no-mtime \
+                --output "~/Music/%(title)s.%(ext)s" \
+                --extract-audio \
+                --audio-format mp3 \
+                $path
 end
