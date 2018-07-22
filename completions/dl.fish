6fbbab09ba6412e084d71eda6a069ba8261e72d5
Dennis Ideler <ideler.dennis@gmail.com>
2018-07-22 16:48:01 +0100

    Add completion for help and version options

--- completions/dl.fish
+++ completions/dl.fish
@@ -1,1 +1,3 @@
-complete -c fish-dl -d "Wrapper around youtube-dl to easily download media"
+complete --command dl --description "Wrapper around youtube-dl to easily download media"
+complete --command dl --short h --long help --description "Display help"
+complete --command dl --short v --long version --description "Display version"
