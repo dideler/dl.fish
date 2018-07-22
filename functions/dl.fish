a57157bbe58b862f5781a353eb3e405b7897335d
Dennis Ideler <ideler.dennis@gmail.com>
2018-07-22 16:43:09 +0100

    Add version flag
    Close #3

--- functions/dl.fish
+++ functions/dl.fish
@@ -1,4 +1,15 @@
+function __dl_print_version
+    echo "dl version 1.0.0"
+end
+
 function dl --description "Opinionated alias for youtube-dl"
+    argparse 'v/version' -- $argv
+
+    if set --query _flag_version
+        __dl_print_version
+        return 0
+    end
+
     for path in $argv
         __dl_download $path
     end
