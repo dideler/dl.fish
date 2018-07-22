e1cffc5e9c7c39de7fa084038bacf3f34e0784a5
Dennis Ideler <ideler.dennis@gmail.com>
2018-07-22 16:45:44 +0100

    Add help option
    Close #2

--- functions/dl.fish
+++ functions/dl.fish
@@ -1,11 +1,26 @@
+function __dl_print_help
+    printf "Usage: dl [OPTION] 'URL'\n\n"
+    printf "Description:\n"
+    printf "    Download media easily from the web\n\n"
+    printf "Examples:\n"
+    printf "    dl 'https://example.com/media-id'\n\n"
+    printf "Options:\n"
+    printf "    -h, --help      Prints helps"
+    printf "    -v, --version   Prints version"
+end
+
 function __dl_print_version
     echo "dl version 1.0.0"
 end
 
 function dl --description "Opinionated alias for youtube-dl"
-    argparse 'v/version' -- $argv
+    set --local options 'h/help' 'v/version'
+    argparse $options -- $argv
 
-    if set --query _flag_version
+    if set --query _flag_help
+        __dl_print_help
+        return 0
+    else if set --query _flag_version
         __dl_print_version
         return 0
     end
