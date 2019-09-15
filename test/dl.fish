59a9b4df88d30dc46cd82e7cbca5c912eeeba6d7
Dennis Ideler <ideler.dennis@gmail.com>
2019-09-16 00:23:02 +0100

    Test dl downloads an MP3 with correct name and location

--- test/dl.fish
+++ test/dl.fish
@@ -1,3 +1,12 @@
-test "$BASENAME"
-    -z ""
+function setup
+    command mkdir -p ~/Music
 end
+
+function teardown
+    command rm -rf ~/Music/quality\ content.mp3
+end
+
+@test "downloads a YouTube video as an MP3 in the Music directory" -e (
+    dl "https://www.youtube.com/watch?v=J1R5S-X9DEw"
+    command ls ~/Music/quality\ content.mp3
+)
