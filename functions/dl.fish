14067c4885aa889e41e7bd1435885b206d5a771a
Dennis Ideler <ideler.dennis@gmail.com>
2021-08-02 02:45:49 +0100

    v1.2.5

--- functions/dl.fish
+++ functions/dl.fish
@@ -16,7 +16,7 @@ function __dl_print_help
 end
 
 function __dl_print_version
-    echo "dl version 1.2.4"
+    echo "dl version 1.2.5"
 end
 
 function dl --description "Opinionated alias for youtube-dl"
